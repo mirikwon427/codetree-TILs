89c3354423b89999acd2eba1a1364a618dd3da2d
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-12 13:32:13 +0900

    Title: 색종이의 총 넓이, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

--- 240412/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240412/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_N = 400;
+    public static final int MAX_R = 200;
     public static final int OFFSET = 100;
 
     public static int n;
-    public static int[][] checked = new int[MAX_N+1][MAX_N+1];
-    public static int x1, y1;
+    public static int[][] checked = new int[MAX_R+1][MAX_R+1];
+    public static int x1, y1, x2, y2;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,21 +20,20 @@ public class Main {
             x1 = OFFSET + Integer.parseInt(st.nextToken());
             y1 = OFFSET + Integer.parseInt(st.nextToken());
 
-            for(int x = x1; x < x1+8; x++) {
-                for(int y = y1+x1; y < y1+x1+8; y++) {
-                    checked[x][y]++;
+            for(int x = x1; x < x1 + 8; x++) {
+                for(int y = y1; y < y1 + 8; y++) {
+                    checked[x][y] = 1;
                 }
             }
         }
         int area = 0;
-        for(int x = x1; x < MAX_N; x++) {
-            for(int y = y1; y < MAX_N; y++) {
-                if(checked[x][y] >= 1) {
+        for(int x = 0; x <= MAX_R; x++) {
+            for(int y = 0; y <= MAX_R; y++) {
+                if(checked[x][y] > 0) {
                     area++;
                 }
             }
         }
         System.out.println(area);
-
     }
 }
