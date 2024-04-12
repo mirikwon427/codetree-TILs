c8d77ac2a25910bb6ac40b64ef1cab365ce803f6
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-12 12:37:36 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 191ms, Memory: 26MB, Status: Passed - CodeTree

--- 240412/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ 240412/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -6,14 +6,13 @@ public class Main {
     public static final int OFFSET = 1000;
 
     public static int[][] checked = new int[MAX_R+1][MAX_R+1];
-    public static int[][] m = new int[MAX_R+1][MAX_R+1];
     public static int x1, y1, x2,y2;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
             x1 = OFFSET + Integer.parseInt(st.nextToken());
             y1 = OFFSET + Integer.parseInt(st.nextToken());
@@ -22,33 +21,16 @@ public class Main {
 
             for(int x = x1; x < x2; x++) {
                 for(int y = y1; y < y2; y++) {
-                    checked[x][y]++;
+                    checked[x][y] = i + 1;
 
                 }
             }
         }
-
-        st = new StringTokenizer(br.readLine());
-
-        x1 = OFFSET + Integer.parseInt(st.nextToken());
-        y1 = OFFSET + Integer.parseInt(st.nextToken());
-        x2 = OFFSET + Integer.parseInt(st.nextToken());
-        y2 = OFFSET + Integer.parseInt(st.nextToken());
-
-        for(int x = x1; x < x2; x++) {
-            for(int y = y1; y < y2; y++) {
-                m[x][y]++;
-            }
-        }
-
 
         int cnt = 0;
         for(int i = 0; i < MAX_R + 1; i++) {
             for(int j = 0; j < MAX_R + 1; j++) {
-                if(checked[i][j] - m[i][j] == 0) {
-                    checked[i][j] = 0;
-                }
-                if(checked[i][j] == 1) {
+                if(checked[i][j] == 1 || checked[i][j] == 2) {
                     cnt++;
                 }
             }
