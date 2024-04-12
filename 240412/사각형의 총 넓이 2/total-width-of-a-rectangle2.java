0168953665007712dbfd748d45cac11fbec2dab7
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-12 11:51:53 +0900

    Title: 사각형의 총 넓이 2, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

--- 240412/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 240412/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -2,11 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_K = 100;
+    public static final int MAX_N = 10;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
 
     public static int n;
-    public static int[] cntW = new int[MAX_K * 2 + 1];
-    public static int[] cntL = new int[MAX_K * 2 + 1];
+    public static int[][] checked = new int[MAX_R+1][MAX_R+1];
+
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,35 +16,29 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
 
-        int cur = MAX_K;
-        int answer = 0;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int x1 = MAX_K + Integer.parseInt(st.nextToken());
-            int y1 = MAX_K + Integer.parseInt(st.nextToken());
-            int x2 = MAX_K + Integer.parseInt(st.nextToken());
-            int y2 = MAX_K + Integer.parseInt(st.nextToken());
+            int x1 = OFFSET + Integer.parseInt(st.nextToken());
+            int y1 = OFFSET + Integer.parseInt(st.nextToken());
+            int x2 = OFFSET + Integer.parseInt(st.nextToken());
+            int y2 = OFFSET + Integer.parseInt(st.nextToken());
 
-            answer += (x2-x1) * (y2-y1);
 
-            for(int j = x1; j < x2; j++) {
-                cntW[j]++;
-            }
-            for(int k = y1; k < y2; k++) {
-                cntL[k]++;
+            for(int x = x1; x < x2; x++) {
+                for(int y = y1; y < y2; y++) {
+                    checked[x][y]++;
+                }
             }
         }
-        int a = 0;
-        int b = 0;
-        for(int i = 0; i < MAX_K * 2; i++) {
-            if(cntW[i] >= 2) {
-                a++;
-            }
-            if(cntL[i] >= 2) {
-                b++;
+        int cnt = 0;
+        for (int i = 0; i < MAX_R; i++) {
+            for (int j = 0; j < MAX_R; j++) {
+                if(checked[i][j] >= 1) {
+                    cnt++;
+                }
             }
         }
-        System.out.print(answer - ( a * b ));
+        System.out.println(cnt);
     }
 }
