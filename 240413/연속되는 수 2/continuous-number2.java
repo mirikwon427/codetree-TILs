d66f5cb8580adc7fdff1d5b46e9d77813c5fc7c3
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-13 15:58:40 +0900

    Title: 연속되는 수 2, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

--- 240413/연속되는 수 2/continuous-number2.java
+++ 240413/연속되는 수 2/continuous-number2.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -10,27 +10,32 @@ public class Main {
 
         int[] a = new int[n];
         int[] b = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             a[i] = Integer.parseInt(st.nextToken());
         }
         int max = 0;
         int cnt = 0;
         for(int i = 0; i < n; i++) {
-            if(i == 0 || a[i] == a[i-1]) {
+            if(i == 0) {
+                cnt = 0;
+            } else if (a[i] == a[i-1]){
                 cnt++;
-            } else {
+            }else {
                 cnt = 0;
             }
-            if(max < cnt) {
-                max = cnt;
+            b[i] = cnt;
+        }
+        for(int i = 0; i < n; i++) {
+            if(max < b[i]) {
+                max = b[i];
             }
         }
         if( n == 1 ) {
             System.out.print(n);
         } else {
-            System.out.print(max + 1);
+
+            System.out.print(max+1);
         }
-        
     }
 }
