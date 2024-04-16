b572a4f26c5fe8d7ab7b1ed18ef1240103780468
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-16 19:15:55 +0900

    Title: 연속되는 수 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240416/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Intege.parseInt(st.nextToken());
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans, cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(i >= 1 && a[i] > a[i-1]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+
+            ans = Math.max(cnt, ans);
+        }
+        
+        System.out.print(ans);
+    }
+}
