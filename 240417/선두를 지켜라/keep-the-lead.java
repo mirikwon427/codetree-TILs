70b132048753f838a4197bf2243b6b121cc6a92e
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-17 12:58:11 +0900

    Title: 선두를 지켜라, Time: 194ms, Memory: 28MB, Status: Passed - CodeTree

--- 240417/선두를 지켜라/keep-the-lead.java
+++ 240417/선두를 지켜라/keep-the-lead.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static final int MAX_N = 1000000;
 
-    public static int n, m, v, t;
+    public static int n, m;
     public static int[] distanceA = new int[MAX_N+1];
     public static int[] distanceB = new int[MAX_N+1];
     public static void main(String[] args) throws IOException{
@@ -14,38 +14,36 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int target1 = 0, target2 = 0;
-
+        int timeA = 1, timeB = 1;
         for(int i = 0; i < n + m; i++) {
             st = new StringTokenizer(br.readLine());
-            v = Integer.parseInt(st.nextToken());
-            t = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
             if(i < n){
-                t += target1;
-                for(int j = target1 + 1; j <= t; j++) {
-                    distanceA[j] = distanceA[j-1] + v;
-                    target1++;
+                while(t --> 0) {
+                    distanceA[timeA] = distanceA[timeA - 1] + v;
+                    timeA++;
                 }
             } else {
-                t += target2;
-                for(int k = target2+1; k <= t; k++) {
-                    distanceB[k] = distanceB[k-1] + v;
-                    target2++;
+                while(t --> 0) {
+                    distanceB[timeB] = distanceB[timeB - 1] + v;
+                    timeB++;
                 }
             }
         }
+        int leader = 0, cnt = 0;;
         int[] ans = new int[MAX_N];
-        for(int i = 1; i < target1; i++) {
+        for(int i = 1; i < timeA; i++) {
             if(distanceA[i] > distanceB[i]){
-                ans[i] = 1;
-            } else {
-                ans[i] = 2;
-            }
-        }
-        int cnt = 0;
-        for(int i = 2; i < target1; i++) {
-            if(ans[i] != ans[i-1]){
-                cnt++;
+                if(leader == 2){
+                    cnt++;
+                }
+                leader = 1;
+            } else if(distanceA[i] < distanceB[i]) {
+                if(leader == 1){
+                    cnt++;
+                }
+                leader = 2;
             }
         }
         System.out.println(cnt);
