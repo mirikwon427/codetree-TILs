9d6f04872dcf408a2178a07c7d9bee2e598c38ed
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-17 13:14:25 +0900

    Title: 좌우로 움직이는 로봇, Time: 92ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240417/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_T = 1000000;
+
+    public static int n, m;
+    public static int[] distanceA = new int[MAX_T+1];
+    public static int[] distanceB = new int[MAX_T+1];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int timeA = 1, timeB = 1;
+        for(int i = 0; i < n + m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            String d = st.nextToken();
+
+            if(i < n) {
+                if(d.equals("R")) {
+                    while(t --> 0) {
+                        distanceA[timeA] = distanceA[timeA - 1] + 1;
+                        timeA++;
+                    }
+                } else {
+                    while(t --> 0) {
+                        distanceA[timeA] = distanceA[timeA - 1] - 1;
+                        timeA++;
+                    }
+                }
+            } else {
+                if(d.equals("R")) {
+                    while(t --> 0) {
+                        distanceB[timeB] = distanceB[timeB - 1] + 1;
+                        timeB++;
+                    }
+                } else {
+                    while(t --> 0) {
+                        distanceB[timeB] = distanceB[timeB - 1] - 1;
+                        timeB++;
+                    }
+                }
+            }
+        }
+        int cnt = 0;
+        for(int i = 1; i <= timeB; i++) {
+            if(distanceA[i] == distanceB[i]) {
+                if(distanceA[i-1] != distanceB[i-1]){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
