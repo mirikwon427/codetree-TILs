3be6ab00c5e125a46e27d77c86c155c8e3756896
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-18 22:13:11 +0900

    Title: 선두를 지켜라 3, Time: 174ms, Memory: 27MB, Status: Passed - CodeTree

--- 240418/선두를 지켜라 3/keep-the-lead-3.java
+++ 240418/선두를 지켜라 3/keep-the-lead-3.java
@@ -3,11 +3,10 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_T = 1000000;
-    public static final int MAX_N = 1000000;
 
     public static int n, m, v, t;
-    public static int[] arrA = new int[MAX_N + 1];
-    public static int[] arrB = new int[MAX_N + 1];
+    public static int[] arrA = new int[MAX_T + 1];
+    public static int[] arrB = new int[MAX_T + 1];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,38 +24,35 @@ public class Main {
                 while(t --> 0) {
                     arrA[target1] = arrA[target1 -1] + v;
                     target1++;
-
                 }
             } else {
                 while(t --> 0) {
                     arrB[target2] = arrB[target2 -1] + v;
                     target2++;
-
                 }
             }
         }
 
-        int[] first = new int[target1];
-        for(int i = 0; i < target1; i++) {
-            if(arrA[i] < arrB[i] ) {
-                first[i] = 2;
-            } else if(arrA[i] > arrB[i]) {
-                first[i] = 1;
-            } else {
-                first[i] = 3;
-            }
-        }
-        int cnt = 0;
-
+        int first = 0, cnt = 0;
         for(int i = 1; i < target1; i++) {
-            if(first[i] == 2 && first[i-1] != 2) {
-                cnt++;
-            } else if(first[i] == 1 && first[i-1] != 1) {
-                cnt++;
-            } else if((first[i] == 3 && first[i-1] != 3)){
-                cnt++;
+            if(arrA[i] < arrB[i] ) {
+                if(first != 2) {
+                    cnt++;
+                }
+                first = 2;
+            } else if(arrA[i] > arrB[i]) {
+                if(first != 1) {
+                    cnt++;
+                }
+                first = 1;
+            } else {
+                if(first != 3) {
+                    cnt++;
+                }
+                first = 3;                
             }
         }
+
         System.out.println(cnt);
 
     }
