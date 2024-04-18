dde10989d2bafdd001db5afa0b218a0b3e39e3d6
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-18 22:08:15 +0900

    Title: 선두를 지켜라 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240418/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_T = 1000;
+    public static final int MAX_N = 1000;
+
+    public static int n, m, v, t;
+    public static int[] arrA = new int[MAX_N + 1];
+    public static int[] arrB = new int[MAX_N + 1];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int target1 = 1, target2 = 1;
+        for(int i = 0; i < n + m; i++) {
+            st = new StringTokenizer(br.readLine());
+            v = Integer.parseInt(st.nextToken());
+            t = Integer.parseInt(st.nextToken());
+
+            if(i < n) {
+                while(t --> 0) {
+                    arrA[target1] = arrA[target1 -1] + v;
+                    target1++;
+
+                }
+            } else {
+                while(t --> 0) {
+                    arrB[target2] = arrB[target2 -1] + v;
+                    target2++;
+
+                }
+            }
+        }
+
+        int[] first = new int[target1];
+        for(int i = 0; i < target1; i++) {
+            if(arrA[i] < arrB[i] ) {
+                first[i] = 2;
+            } else if(arrA[i] > arrB[i]) {
+                first[i] = 1;
+            } else {
+                first[i] = 3;
+            }
+        }
+        int cnt = 0;
+
+        for(int i = 1; i < target1; i++) {
+            if(first[i] == 2 && first[i-1] != 2) {
+                cnt++;
+            } else if(first[i] == 1 && first[i-1] != 1) {
+                cnt++;
+            } else if((first[i] == 3 && first[i-1] != 3)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
