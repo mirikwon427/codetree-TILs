f23dea9809183fb3ca7bbc1fc6e76ce615a331d4
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-19 15:12:24 +0900

    Title: 방향에 맞춰 이동, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240419/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_N = 10000;
+
+    public static int n;
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br. readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int x = 0, y = 0;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br. readLine());
+            String d = st.nextToken();
+            int v = Integer.parseInt(st.nextToken());
+
+            if(d.equals("N")) {
+                y += v;
+            } else if(d.equals("E")) {
+                x += v;
+            } else if(d.equals("S")) {
+                y -= v;
+            } else {
+                x -= v;
+            }
+        }
+        System.out.print(x + " " + y);
+    }
+}
