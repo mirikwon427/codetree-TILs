9036429e9dd677e284c758535af7f9b1943bafb3
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-19 15:15:58 +0900

    Title: 방향에 맞춰 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240419/방향에 맞춰 이동/move-in-direction.java
+++ 240419/방향에 맞춰 이동/move-in-direction.java
@@ -4,27 +4,32 @@ import java.io.*;
 public class Main {
     public static final int MAX_N = 10000;
 
-    public static int n;
+    public static int n, x, y;
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br. readLine());
 
         n = Integer.parseInt(st.nextToken());
-        int x = 0, y = 0;
+
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br. readLine());
             String d = st.nextToken();
             int v = Integer.parseInt(st.nextToken());
 
-            if(d.equals("N")) {
-                y += v;
-            } else if(d.equals("E")) {
-                x += v;
-            } else if(d.equals("S")) {
-                y -= v;
+            int dir;
+            if(d == 'E'){
+                dir = 0;
+            } else if(d == 'W') {
+                dir = 1;
+            } else if(d == 'S') {
+                dir = 2;
             } else {
-                x -= v;
+                dir = 3;
             }
+            x += dx[dir] * v;
+            y += dx[dir] * v;
         }
         System.out.print(x + " " + y);
     }
