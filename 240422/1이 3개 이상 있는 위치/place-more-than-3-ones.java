12eeaba9df558e82fea939ac0c4a2a0888b234f8
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-22 21:23:52 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240422/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int cnt = 0, ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    int nx = i + dx[k], ny = j + dy[k];
+
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                        continue;
+                    }
+                    if(arr[nx][ny] == 1) {
+                        cnt++;
+                    }
+                }
+                if( cnt >= 3 ) {
+                    ans++;
+                }
+                cnt = 0;
+            }
+
+        }
+        System.out.print(ans);
+    }
+}
