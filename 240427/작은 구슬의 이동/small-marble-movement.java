848b8a4bce80aea6c04fc80c374109951498cfa0
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-27 09:36:18 +0900

    Title: 작은 구슬의 이동, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240427/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n,c,t,r;
+    public static String d;
+
+    public static int[] dx = new int[]{0, 1, -1, 0};
+    public static int[] dy = new int[]{1, 0, 0, -1};
+
+    public static boolean inRange(int x, int y) {
+        return 0 < x && x < n && 0 < y && y < n ;
+    }
+    public static int getDir(char a) {
+        if(a == 'R')
+            return 0;
+        else if(a == 'D')
+            return 1;
+        else if(a == 'U')
+            return 2;
+        else
+            return 3;
+    }
+    public static int getOppoDir(char a) {
+        if(a == 'R')
+            return 3;
+        else if(a == 'D')
+            return 2;
+        else if(a == 'U')
+            return 1;
+        else
+            return 0;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = st.nextToken();
+
+        int x, cnt = 0;
+        for(int i = 0; i <= t; i++){
+            if(inRange(r,c) && cnt == 0) {
+                x = getDir(d.charAt(0));
+                r += dx[x];
+                c += dy[x];
+            } else {
+                x = getOppoDir(d.charAt(0));
+                r += dx[x];
+                c += dy[x];
+                cnt++;
+            }
+        }
+        System.out.print(r + " " + c);
+    }
+}
