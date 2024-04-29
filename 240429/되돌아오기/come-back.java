42222b88b0206a350e530bb14a0a34f98183edba
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-29 15:28:59 +0900

    Title: 되돌아오기, Time: 106ms, Memory: 9MB, Status: Passed - CodeTree

--- 240429/되돌아오기/come-back.java
+++ 240429/되돌아오기/come-back.java
@@ -2,10 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_T = 100;
+    public static final int MAX_T = 200;
     public static int[][] answer = new int[MAX_T+1][MAX_T+1];
     public static int n,nx,ny;
-    public static int dirNum = 0, x = 10, y = 10;
+    public static int dirNum = 0, x = 100, y = 100;
     public static int[] dx = {0, 1, 0, -1};
     public static int[] dy = {1, 0, -1, 0};
     public static int direction(char a) {
@@ -24,7 +24,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        int cnt = 2;
+        int cnt = 2, ans = 0;
         answer[x][y] = 1;
         for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -42,10 +42,14 @@ public class Main {
                 cnt++;
             }
             if (answer[nx][ny] == 1) {
+                ans = -1;
                 break;
             }
         }
-        System.out.print(cnt -1);
-
+        if( ans == -1) {
+            System.out.print(cnt -1);
+        } else {
+            System.out.println(-1);
+        }
     }
 }
