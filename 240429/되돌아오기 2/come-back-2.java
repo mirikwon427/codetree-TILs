1f98fb23eba470ec7dd2e59a045a56e09c4ed177
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-04-29 17:25:40 +0900

    Title: 되돌아오기 2, Time: 149ms, Memory: 11MB, Status: Passed - CodeTree

--- 240429/되돌아오기 2/come-back-2.java
+++ 240429/되돌아오기 2/come-back-2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main {
-    public static int x, y, elapsedTime;
+    public static int x = 1, y = 0, elapsedTime = 1;
     public static int ans = -1, dirNum = 0;
     public static int[] dx = new int[]{1, 0, -1, 0};
     public static int[] dy = new int[]{0, 1, 0, -1};
@@ -10,19 +10,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String dir = br.readLine();
-        for(int i = 0; i < dir.length(); i++) {
+
+        for(int i = 1; i < dir.length(); i++) {
+            elapsedTime++;
             if(dir.charAt(i) == 'L') {
                 dirNum = (dirNum + 3) % 4;
+                continue;
             }
             if(dir.charAt(i) == 'R') {
                 dirNum = (dirNum + 1) % 4;
+                continue;
             }
             x += dx[dirNum];
             y += dy[dirNum];
 
-            elapsedTime++;
-
-            if(x == -1 && y == 0) {
+            if(x == 0 && y == 0) {
                 ans = elapsedTime;
                 break;
             }
