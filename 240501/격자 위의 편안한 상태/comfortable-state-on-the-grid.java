3c50eabec91c3412642ff0a711e95e670bd9fcd3
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-01 13:30:38 +0900

    Title: 격자 위의 편안한 상태, Time: 284ms, Memory: 12MB, Status: Passed - CodeTree

--- 240501/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
+++ 240501/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -3,30 +3,25 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_N = 100;
-    public static int[][] answer = new int[MAX_N + 1][MAX_N + 1];
+    public static final int dirNum = 4;
+    public static int[][] answer = new int[MAX_N][MAX_N];
     public static int n, m, r, c;
-    public static int dirNum = 4;
     public static int[] dx = new int[]{1, 0, -1, 0};
     public static int[] dy = new int[]{0, 1, 0, -1};
 
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < n;
     }
-    public static boolean dir(int r, int c) {
+    public static int dir(int r, int c) {
         int cnt = 0;
         for(int i = 0; i < dirNum; i++) {
             int nx = r + dx[i];
             int ny = c + dy[i];
-            if(inRange(nx, ny)) {
-                if(answer[nx][ny] == 1) {
-                    cnt++;
-                }
-            }
-            if(cnt >= 3) {
-                return true;
+            if(inRange(nx, ny) && answer[nx][ny] == 1) {
+                cnt++;
             }
         }
-        return false;
+        return cnt;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,7 +30,6 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int[] result = new int[m];
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
@@ -44,15 +38,12 @@ public class Main {
             r--; c--;
             answer[r][c] = 1;
 
-            boolean done = dir(r,c);
-            if(done) {
-                result[i] = 1;
+            int done = dir(r,c);
+            if(done == 3) {
+                System.out.println(1);
             } else {
-                result[i] = 0;
+                System.out.println(0);
             }
-        }
-        for(int i = 0; i < m; i++) {
-            System.out.println(result[i]);
-        }
+        } 
     }
 }
