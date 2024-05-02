6b73a3b86c9925936cdd399f227915dc87b16583
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-02 12:08:57 +0900

    Title: 거울에 레이저 쏘기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240502/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240502/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -1,77 +1,75 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
     public static final int MAX_N = 1000;
-    public static int n,cnt, dirNum = 0, nx, ny,start;
-    public static int[][] arr = new int[MAX_N][MAX_N];
-    public static int[] dx = {1, 0, -1, 0};
-    public static int[] dy = {0, 1, 0, -1};
+
+    public static int n;
+    public static char[][] arr = new char[MAX_N][MAX_N];
+
+    public static int startNum;
+    public static int x, y,moveDir;
+    
+    public static void initalize(int num){
+        if(num <= n) {
+            x = 0; y = num - 1; moveDir = 0;
+        } else if(num <= n * 2) {
+            x = num - n - 1; y = n - 1; moveDir = 1;
+        } else if(num <= n * 3) {
+            x = n - 1; y = n - (num - 2 * n); moveDir = 2;
+        } else if(num < n * 4) {
+            x = n - (num - 3 * n); y = 0; moveDir = 3;
+        }
+    }
+    public static void move(nextDir){
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, -1, 0, 1};
+
+        x += dx[nextDir];
+        y += dy[nextDir];
+
+        moveDir = nextDir;
+    }
+
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < n;
     }
-    public static int startNum (int start) {
-        int result = 0, x=0, y=0;
-        if(start < n) {
-            result = no(0, x = 0, y =start-1);
-        }else if(n < start && start < n*2){
-            result = no(1, x = start-n-1, y =n-1);
-        }else if(n*2 < start && start < n*3){
-            result = no(2, x = n-1, y =start-(n*2)-1);
-        }else if(n*3 < start && start < n*4){
-            result = no(3, x = start-(n*3)-1, y = 0);
-        }
-        return result;
-    }
-    public static  int no (int dirNum, int x, int y){
-        for(int i = 0; i < n; i++) {
-            nx = x;
-            ny = y;
-            if(!inRange(nx,ny)){
-                break;
-            }
-            if(arr[nx][ny] == 1){
-                if(dirNum == 1){
-                    dirNum = (dirNum +1) % 4;
-                } else {
-                    dirNum = (dirNum +3) % 4;
-                }
-                cnt++;
+
+    public static int simulate(){
+        int moveNum = 0;
+        while(inRange(x,y)) {
+            if(arr[x][y] == '/') {
+                move(moveDir ^ 1);
             } else {
-                if(dirNum == 1){
-                    dirNum = (dirNum +3) % 4;
-                } else {
-                    dirNum = (dirNum +1) % 4;
-                }
-                cnt++;
+                move(3 - moveDir);
             }
-            x += dx[dirNum];
-            y += dy[dirNum];
+            moveNum++;
         }
-        return cnt;
+        return moveNum;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        int length = n*n;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+
+            String input = st.nextToken();
+
             for(int j = 0; j < n; j++) {
-                char line1 = line.charAt(j);
-                if(line1 == ('/')) {
-                    arr[i][j] = 1;
-                } else {
-                    arr[i][j] = 2;
-                }
+                arr[i][j] = input.charAt(j);
             }
         }
-        st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken());
-        int answer = startNum(start);
 
-        System.out.println(answer);
+        startNum = Integer.parseInt(st.nextToken());
 
+        initalize(startNum);
+
+        int moveNum = simulate();
+
+        System.out.print(moveNum);
+        
     }
 }
