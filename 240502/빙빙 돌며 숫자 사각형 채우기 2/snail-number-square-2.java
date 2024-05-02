c4f825b9d843c89bb7c671d7cc6dbb64eb365755
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-02 13:43:54 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 226ms, Memory: 11MB, Status: Passed - CodeTree

--- 240502/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 240502/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -1,57 +1,39 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static final int DIR_NUM = 4;
     public static final int MAX_N = 100;
-    
+
     public static int n, m;
+    public static int x = 0, y = 0, moveNum = 1, dirNum = 0;
     public static int[][] arr = new int[MAX_N][MAX_N];
-    
-    public static int[] dx = new int[]{0, 1,  0, -1};
-    public static int[] dy = new int[]{1, 0, -1,  0};
-    
-    public static int currX, currY;  // 시작은 (0, 0) 입니다.
-    public static int dir = 1;       // 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
-    
-    public static boolean inRange(int x, int y){
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, 1, 0, -1};
+
+    public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < m;
     }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        m = sc.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // 처음 시작 위치에 초기값을 적습니다.
-        arr[currX][currY] = 1;
-
-        // n*m개의 숫자를 적어야 합니다. 
-        for(int i = 2; i <= n * m; i++) { // 숫자 i를 어디에 적을지 결정합니다.
-            while(true) { // 나아갈 수 있을때까지 방향을 바꿔가며 확인해봅니다. 
-                // 현재 방향 dir를 기준으로 그 다음 위치 값을 계산합니다.
-                int nx = currX + dx[dir];
-                int ny = currY + dy[dir];
-                
-                // 그 위치로 나아갈 수 있는지 확인합니다.
-                if(inRange(nx, ny) && arr[nx][ny] == 0) {
-                    // 나아갈 수 있다면 위치를 갱신해주고 배열에 올바른 값을 채워넣습니다.
-                    currX = nx; 
-                    currY = ny;
-                    arr[currX][currY] = i;
-                    break;
-                } else {
-                    // 나아갈 수 없다면 반시계방향으로 90'를 회전하여 
-                    // 그 다음 방향을 확인해봐야 합니다.
-                    dir = (dir + 3) % 4;
-                }
+        arr[x][y] = 1;
+        for(int i = 2; i <= n * m; i++) {
+            int nx = x + dx[dirNum]; int ny = y +dy[dirNum];
+            if(!inRange(nx, ny) || arr[nx][ny] != 0){
+                dirNum = (dirNum + 1) % 4;
             }
+            x = x + dx[dirNum]; y = y + dy[dirNum];
+            arr[x][y] = i;
         }
-        
-        // 출력:
+
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) 
+            for(int j = 0; j <m; j++) {
                 System.out.print(arr[i][j] + " ");
+            }
             System.out.println();
         }
     }
