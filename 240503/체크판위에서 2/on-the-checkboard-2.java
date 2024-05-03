e7afa32ccc5f8bdde797bf2a3d81f3454a50268a
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-03 14:21:39 +0900

    Title: 체크판위에서 2, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240503/체크판위에서 2/on-the-checkboard-2.java
+++ 240503/체크판위에서 2/on-the-checkboard-2.java
@@ -4,10 +4,8 @@ import java.io.*;
 public class Main {
     public static final int MAX_R = 15;
     public static int r, c;
-    public static String[][] arr = new String[MAX_R][MAX_R];
+    public static char[][] grid = new char[MAX_R][MAX_R];
 
-    public static int[] dx = {0, 1};
-    public static int[] dy = {1, 0};
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,20 +17,19 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < c; j++) {
                 String input = st.nextToken();
-                arr[i][j] = input;
+                grid[i][j] = input.charAt(0);
             }
         }
         int cnt = 0;
-        for(int i = 0; i < r - 1; i++) {
-            for(int j = 0; j < c - 2; j++) {
-                if(arr[i][j].equals("W")) {
-                    if(arr[i + 1][j + 2].equals("B")){
-                        cnt++;
-                    }
-                }
-                if(arr[i][j].equals("B")) {
-                    if(arr[i + 1][j + 2].equals("W")){
-                        cnt++;
+        for(int i = 1; i < r; i++) {
+            for(int j = 1; j < c; j++) {
+                for(int k = i + 1; k < r - 1; k++) {
+                    for(int l = j + 1; l < c -1; l++) {
+                        if(grid[0][0] != grid[i][j] &&
+                                grid[i][j] != grid[k][l] &&
+                                grid[k][l] != grid[r - 1][c - 1]) {
+                            cnt++;
+                        }
                     }
                 }
             }
