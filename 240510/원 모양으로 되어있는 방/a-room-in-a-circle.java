5a4af8069235c949beaf7e9206b93f1d578893b8
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-10 12:11:28 +0900

    Title: 원 모양으로 되어있는 방, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

--- 240510/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 240510/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -5,7 +5,7 @@ public class Main {
     public static int n;
     public static final int MAX_N = 1000;
     public static final int INT_MAX = Integer.MAX_VALUE;
-    public static int[] people = new int[MAX_N];
+    public static int[] arr = new int[MAX_N];
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,27 +13,18 @@ public class Main {
 
         for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            people[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = INT_MAX;
+        int ans = INT_MAX;
         for(int i = 0; i < n; i++) {
-            int prevIdx = i;
-            int ans = 0;
+            int sumDist = 0;
             for(int j = 0; j < n; j++) {
-                if(i == (n-j)) {
-                    int idx = j;
-                    for(int x = 0; x < n-j; x++) {
-                        ans += people[x] * idx;
-                        idx++;
-                    }
-                    break;
-                }
-                ans += people[prevIdx] * j;
-                prevIdx++;
+                int dist = ( j + n - i ) % n;
+                sumDist += arr[j] * dist;
             }
-            result = Math.min(ans, result);
+            ans = Math.min(sumDist, ans);
         }
-        System.out.print(result);
+        System.out.print(ans);
     }
 }
