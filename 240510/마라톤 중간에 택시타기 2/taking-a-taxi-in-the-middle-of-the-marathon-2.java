8b0365929602c3989852b2e2176a0b5d6b1e1c0e
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-05-10 09:40:02 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240510/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 240510/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -1,36 +1,39 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
     public static final int MAX_N = 100;
     
     public static int n;
     public static int[] x = new int[MAX_N];
     public static int[] y = new int[MAX_N];
-    public static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        n = sc.nextInt();
         for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
         }
-
+        
+        // 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+        int ans = INT_MAX;
         for(int i = 1; i < n - 1; i++) {
-            int prevIdx = 0;
+            // 거리를 구합니다.
             int dist = 0;
+            int prevIdx = 0;
             for(int j = 1; j < n; j++) {
-                if(j == i){
-                    continue;
-                }
-                dist += Math.abs(x[prevIdx]-x[j]) + Math.abs(y[prevIdx] + y[j]);
+                if(j == i) continue;
+                dist += Math.abs(x[prevIdx] - x[j]) + Math.abs(y[prevIdx] - y[j]);
                 prevIdx = j;
             }
-            min = Math.min(min, dist);
+            
+            // 가능한 거리 중 최솟값을 구해줍니다.
+            ans = Math.min(ans, dist);
         }
-        System.out.println(min);
+        
+        // 출력
+        System.out.print(ans);
     }
 }
