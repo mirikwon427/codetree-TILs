f2f96b3c8bfa09896ad216b48613880ed8a61836
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-09-08 11:03:35 +0900

    [Silver III] Title: 칸토어 집합, Time: 36 ms, Memory: 32500 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,15 @@
+def func(k):
+    #base case
+    if(k == 0):
+        return '-'
+    
+    #recursive case
+    return func(k-1) + (' ' * (3 ** (k-1))) + func(k-1)
+
+while True:
+    try:
+        N = int(input())
+        print(func(N))
+    except:
+        break
+    
