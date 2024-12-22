f2423540b18b62ae05251b0a0e1ab7080d550151
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-12-23 06:06:06 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,17 +1,14 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input().strip())
+
 digits = len(str(N))
 
-start = max(1, N - (9 * digits))
+start = max(1, (N - (9 * digits)))
 
-
-for i in range(start, N + 1):
-	digit_sum = i + sum(int(d) for d in str(i))
-
-	if digit_sum == N:
-		print(i)
-		break;
+for i in range(start, N):
+    digit_sum = i + sum(int(d) for d in str(i))
+    
+    if digit_sum == N:
+        print(i)
+        break
 else:
-	print(0)
+    print(0)
