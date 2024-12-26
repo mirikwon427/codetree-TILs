9dfb71490ffdc798a8eae3a92a0e6a8d1126ee36
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2024-12-26 21:29:36 +0900

    [Gold IV] Title: 최단경로, Time: 1148 ms, Memory: 78712 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,28 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+from queue import PriorityQueue
+INF = int(1e12)
+
+V, E = map(int, input().split())
+K = int(input())
+adj_list = [[] for _ in range(V + 1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    adj_list[u].append([v, w])
+
+pq = PriorityQueue()
+dist = [INF] * (V + 1)
+dist[K] = 0
+pq.put([0, K])
+
+while not pq.empty():
+    cur_dist, cur_node = pq.get()
+    for adj_node, adj_dist in adj_list[cur_node]:
+        temp_dist = cur_dist + adj_dist
+        if temp_dist < dist[adj_node]:
+            dist[adj_node] = temp_dist
+            pq.put([temp_dist, adj_node])
+            
+for d in dist[1:]:
+    print(d if d != INF else 'INF')
