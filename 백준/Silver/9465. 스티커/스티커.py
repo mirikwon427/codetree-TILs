3586ecb10a5cf3a7b1113d1ca3aac5bdae48623b
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-01-12 02:20:46 +0900

    [Silver I] Title: 스티커, Time: 668 ms, Memory: 50240 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,26 @@
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    scores = [list(map(int, input().split())) for _ in range(2)]
+    dp = [[0] * n for _ in range(2)]
+    
+    dp[0][0] = scores[0][0]
+    dp[1][0] = scores[1][0]
+    
+    if n == 1:
+        print(max(dp[0][0], dp[1][0]))
+        continue
+        
+    dp[0][1] = scores[1][0] + scores[0][1]
+    dp[1][1] = scores[0][0] + scores[1][1]
+    
+    if n == 2:
+        print(max(dp[0][1], dp[1][1]))
+        continue
+    
+    for i in range(2, n):
+        dp[0][i] = max(dp[1][i - 2], dp[1][i - 1]) + scores[0][i]
+        dp[1][i] = max(dp[0][i - 2], dp[0][i - 1]) + scores[1][i]
+    
+    print(max(dp[0][-1], dp[1][-1]))
+        
