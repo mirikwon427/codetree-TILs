d1380b9dde95090d44c56f64719d2181685efce7
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-01-13 00:45:47 +0900

    [Gold V] Title: AC, Time: 220 ms, Memory: 39936 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,23 +1,23 @@
 import sys
-from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
+from collections import deque
 
 T = int(input())
 
 for _ in range(T):
-    p = input()
-    n = int(input())
+    case = input()
+    N = int(input())
     x = input().strip()
-
+    
     if x == "[]":
         arr = deque()
     else:
         arr = deque(map(int, x[1:-1].split(',')))
-
+    
     is_reversed = False
     error_flag = False
-
-    for command in p:
+    
+    for command in case:
         if command == 'R':
             is_reversed = not is_reversed
         elif command == 'D':
@@ -29,7 +29,7 @@ for _ in range(T):
                 arr.pop()
             else:
                 arr.popleft()
-
+        
     if not error_flag:
         if is_reversed:
             print("[" + ",".join(map(str, reversed(arr))) + "]")
