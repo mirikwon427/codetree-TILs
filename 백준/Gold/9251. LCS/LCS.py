b9d359f01da63c455d60788e326828f5385b8478
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-01-18 01:01:42 +0900

    [Gold V] Title: LCS, Time: 444 ms, Memory: 57628 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+arr1 = input().strip()
+arr2 = input().strip()
+
+n, m = len(arr1), len(arr2)
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if arr1[i - 1] == arr2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(dp[n][m])
