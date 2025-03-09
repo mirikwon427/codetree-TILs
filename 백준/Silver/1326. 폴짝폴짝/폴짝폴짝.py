ad9997aed977d6ef39564b00a3a06cdc9f387127
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-03-09 16:23:19 +0900

    [Silver II] Title: 폴짝폴짝, Time: 56 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
@@ -3,31 +3,35 @@ input = sys.stdin.readline
 from collections import deque
 
 def bfs():
+    global visited
+
+    visited[a-1] = 0
     q = deque()
-    visited = [-1 for _ in range(N)]
-    
-    q. append(a - 1)
-    visited[a - 1] = 0
-    
+    q.append(a - 1)
+
     while q:
         now = q.popleft()
-        
+
         for i in range(now, N, arr[now]):
             if visited[i] == -1:
-                q.append(i)
                 visited[i] = visited[now] + 1
+                q.append(i)
                 if i == b - 1:
                     return visited[i]
+
         for i in range(now, -1, -arr[now]):
             if visited[i] == -1:
-                q.append(i)
                 visited[i] = visited[now] + 1
+                q.append(i)
                 if i == b - 1:
                     return visited[i]
     return -1
 
+
 N = int(input())
 arr = list(map(int, input().split()))
 a, b = map(int, input().split())
+
+visited = [-1] * N
 
 print(bfs())
