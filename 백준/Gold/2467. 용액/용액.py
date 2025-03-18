3815a66306fe0a07fb747966bdcadc5f1aa679b2
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-03-18 19:39:52 +0900

    [Gold V] Title: 용액, Time: 120 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+solutions = list(map(int, input().split()))
+
+left, right = 0, N - 1
+sum = float('inf')
+pair = (solutions[left], solutions[right])
+
+while left < right:
+    mix = solutions[left] + solutions[right]
+
+    if abs(mix) <abs(sum):
+        sum = mix
+        pair = (solutions[left], solutions[right])
+
+    if mix == 0:
+        break
+
+    if mix < 0:
+        left += 1
+    else:
+        right -= 1
+
+print(pair[0], pair[1])
