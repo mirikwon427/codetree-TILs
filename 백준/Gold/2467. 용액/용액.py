e2b3419c42f5f212408f7f659355fabdedee2fa7
mirikwon427 <149026894+mirikwon427@users.noreply.github.com>
2025-03-18 20:29:08 +0900

    [Gold V] Title: 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -10,14 +10,14 @@ pair = (solutions[left], solutions[right])
 
 while left < right:
     mix = solutions[left] + solutions[right]
-
-    if abs(mix) <abs(sum):
+    
+    if abs(mix) < abs(sum):
         sum = mix
         pair = (solutions[left], solutions[right])
-
+    
     if mix == 0:
         break
-
+    
     if mix < 0:
         left += 1
     else:
